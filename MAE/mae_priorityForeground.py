857fa91526e09abff2a1466e70bbc8dec7ffa59e
jshfu <jshfu@naver.com>
2024-03-19 23:51:09 +0900

    Add: mae_priorityForeground.py

---
+++ MAE/mae_priorityForeground.py
@@ -0,0 +1,337 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# DeiT: https://github.com/facebookresearch/deit
+# MAE: https://github.com/facebookresearch/mae
+# --------------------------------------------------------
+
+from functools import partial
+
+import torch
+import torch.nn as nn
+from torchvision import transforms
+
+from util import video_vit
+from custom_resnet import r2plus1d_18
+
+from SemanticGuidedHumanMatting.model.model import HumanMatting
+from SemanticGuidedHumanMatting import inference
+from einops import rearrange, reduce
+from collections import OrderedDict
+
+# from mae_st.util.logging import master_print as print
+
+class MaskedAutoencoderViT(nn.Module):
+    """Masked Autoencoder with VisionTransformer backbone"""
+
+    def __init__(
+        self,
+        mask_ratio,
+        img_size=224,
+        patch_size=16,
+        in_chans=3,
+        embed_dim=96,
+        depth=24,
+        num_heads=16,
+        decoder_embed_dim=512,
+        decoder_depth=8,
+        decoder_num_heads=16,
+        mlp_ratio=4.0,
+        norm_layer=nn.LayerNorm,
+        norm_pix_loss=False,
+        num_frames=15,
+        t_patch_size=3,
+        patch_embed=video_vit.PatchEmbed,
+        no_qkv_bias=False,
+        sep_pos_embed=False,
+        trunc_init=False,
+        cls_embed=False,
+        pred_t_dim=8,
+        **kwargs,
+    ):
+        super().__init__()
+                
+        self.avgpool3d = nn.AvgPool3d
+        self.mask_ratio = mask_ratio
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_frames = num_frames
+        self.t_patch_size = t_patch_size
+        
+        
+        self.trunc_init = trunc_init
+        self.sep_pos_embed = sep_pos_embed
+        self.cls_embed = cls_embed
+        self.pred_t_dim = pred_t_dim
+        self.t_pred_patch_size = t_patch_size * pred_t_dim // num_frames
+
+        self.patch_embed = patch_embed(
+            img_size,
+            patch_size,
+            in_chans,
+            embed_dim,
+            num_frames,
+            t_patch_size,
+        )
+        num_patches = self.patch_embed.num_patches
+        input_size = self.patch_embed.input_size
+        self.input_size = input_size
+
+        if self.cls_embed:
+            self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+            self.decoder_cls_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
+
+        if sep_pos_embed:
+            self.pos_embed_spatial = nn.Parameter(
+                torch.zeros(1, input_size[1] * input_size[2], embed_dim)
+            )
+            self.pos_embed_temporal = nn.Parameter(
+                torch.zeros(1, input_size[0], embed_dim)
+            )
+            if self.cls_embed:
+                self.pos_embed_class = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        else:
+            if self.cls_embed:
+                _num_patches = num_patches + 1
+            else:
+                _num_patches = num_patches
+
+            self.pos_embed = nn.Parameter(
+                torch.zeros(1, _num_patches, embed_dim),
+            )
+
+        self.blocks = nn.ModuleList(
+            [
+                video_vit.Block(
+                    embed_dim,
+                    num_heads,
+                    mlp_ratio,
+                    qkv_bias=not no_qkv_bias,
+                    qk_scale=None,
+                    norm_layer=norm_layer,
+                )
+                for i in range(depth)
+            ]
+        )
+        self.norm = norm_layer(embed_dim)
+        self.dropout = nn.Dropout(0.5)
+        self.head = nn.Linear(embed_dim, 5)
+        
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
+
+        if sep_pos_embed:
+            self.decoder_pos_embed_spatial = nn.Parameter(
+                torch.zeros(1, input_size[1] * input_size[2], decoder_embed_dim)
+            )
+            self.decoder_pos_embed_temporal = nn.Parameter(
+                torch.zeros(1, input_size[0], decoder_embed_dim)
+            )
+            if self.cls_embed:
+                self.decoder_pos_embed_class = nn.Parameter(
+                    torch.zeros(1, 1, decoder_embed_dim)
+                )
+        else:
+            if self.cls_embed:
+                _num_patches = num_patches + 1
+            else:
+                _num_patches = num_patches
+
+            self.decoder_pos_embed = nn.Parameter(
+                torch.zeros(1, _num_patches, decoder_embed_dim),
+            )
+
+        self.norm_pix_loss = norm_pix_loss
+
+        self.initialize_weights()
+
+        # print("model initialized")
+
+    def initialize_weights(self):
+        if self.cls_embed:
+            torch.nn.init.trunc_normal_(self.cls_token, std=0.02)
+        if self.sep_pos_embed:
+            torch.nn.init.trunc_normal_(self.pos_embed_spatial, std=0.02)
+            torch.nn.init.trunc_normal_(self.pos_embed_temporal, std=0.02)
+
+            if self.cls_embed:
+                torch.nn.init.trunc_normal_(self.pos_embed_class, std=0.02)
+        else:
+            torch.nn.init.trunc_normal_(self.pos_embed, std=0.02)
+        w = self.patch_embed.proj.weight.data
+        if self.trunc_init:
+            torch.nn.init.trunc_normal_(w)
+            torch.nn.init.trunc_normal_(self.mask_token, std=0.02)
+        else:
+            torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+            torch.nn.init.normal_(self.mask_token, std=0.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            if self.trunc_init:
+                nn.init.trunc_normal_(m.weight, std=0.02)
+            else:
+                torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    
+    def background_masking(self, x, segmap, mask_ratio):
+        N, L, D = x.shape
+        segmap = segmap.reshape(N,-1)
+        len_keep = int(L*(1 - mask_ratio))
+        
+        sorted_values, sorted_indices = torch.sort(segmap, dim=1, descending=True)
+        
+        top_indices = sorted_indices[:, :len_keep]
+        
+        selected_values = torch.gather(x, dim=1, index=top_indices.unsqueeze(-1).repeat(1, 1, D))
+
+        return selected_values, top_indices
+        
+    
+
+    def forward_encoder(self, x, segmap, mask_ratio):
+        # embed patches
+        _, _, _D, _, _ = x.shape
+        
+        x = self.patch_embed(x)
+        
+        N, T, L, C = x.shape
+
+        x = x.reshape(N, T * L, C)
+
+        segmap = self.avgpool3d((self.t_patch_size,self.patch_size,self.patch_size), stride=(self.t_patch_size,self.patch_size,self.patch_size))(torch.Tensor(segmap).squeeze(dim=-1))
+
+        # masking: length -> length * mask_ratio
+        x, ids_keep = self.background_masking(x, segmap, mask_ratio)
+        x = x.view(N, -1, C)
+        # append cls token
+        if self.cls_embed:
+            cls_token = self.cls_token
+            cls_tokens = cls_token.expand(x.shape[0], -1, -1)
+            x = torch.cat((cls_tokens, x), dim=1)
+
+        # add pos embed w/o cls token
+        if self.sep_pos_embed:
+            pos_embed = self.pos_embed_spatial.repeat(
+                1, self.input_size[0], 1
+            ) + torch.repeat_interleave(
+                self.pos_embed_temporal,
+                self.input_size[1] * self.input_size[2],
+                dim=1,
+            )
+            pos_embed = pos_embed.expand(x.shape[0], -1, -1)
+            pos_embed = torch.gather(
+                pos_embed,
+                dim=1,
+                index=ids_keep.unsqueeze(-1).repeat(1, 1, pos_embed.shape[2]),
+            )
+            if self.cls_embed:
+                pos_embed = torch.cat(
+                    [
+                        self.pos_embed_class.expand(pos_embed.shape[0], -1, -1),
+                        pos_embed,
+                    ],
+                    1,
+                )
+        else:
+            if self.cls_embed:
+                cls_ind = 1
+            else:
+                cls_ind = 0
+            pos_embed = self.pos_embed[:, cls_ind:, :].expand(x.shape[0], -1, -1)
+            pos_embed = torch.gather(
+                pos_embed,
+                dim=1,
+                index=ids_keep.unsqueeze(-1).repeat(1, 1, pos_embed.shape[2]),
+            )
+            if self.cls_embed:
+                pos_embed = torch.cat(
+                    [
+                        self.pos_embed[:, :1, :].expand(x.shape[0], -1, -1),
+                        pos_embed,
+                    ],
+                    1,
+                )
+        x = x.view([N, -1, C]) + pos_embed
+
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+
+        if self.cls_embed:
+            # remove cls token
+            x = x[:, 1:, :]
+        else:
+            x = x[:, :, :]
+            
+        # classifier
+        x = x[:, 1:, :].mean(dim=1)  # global pool
+        x = self.norm(x)
+        # x = self.fc_norm(x)
+        x = self.dropout(x)
+        x = self.head(x)
+
+        return x
+    
+        # return x, mask, ids_restore
+
+
+    def forward(self, imgs, segmap):
+        # imgs = self.r2plus1d_backbone(imgs)
+        
+        x = self.forward_encoder(imgs, segmap, self.mask_ratio)
+
+        return x
+
+
+def mae_vit_base_patch16(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16,
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        **kwargs,
+    )
+    return model
+
+
+def mae_vit_large_patch16(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16,
+        embed_dim=1024,
+        depth=24,
+        num_heads=16,
+        mlp_ratio=4,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        **kwargs,
+    )
+    return model
+
+
+def mae_vit_huge_patch14(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=14,
+        embed_dim=1280,
+        depth=32,
+        num_heads=16,
+        mlp_ratio=4,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        **kwargs,
+    )
+    return model
